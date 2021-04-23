b86f65b7c69bf6bb74be0c6d63068aa5bd8034dc
Kevin Strathdee <kevin.strathdee@jambit.com>
2021-04-23 13:45:02 +0200

    Add page transitions

--- lib/pages/routes/router.dart
+++ lib/pages/routes/router.dart
@@ -14,12 +14,38 @@ Route onGenerateRoute(RouteSettings settings) {
     case '/profile':
       return MaterialPageRoute(builder: (_) => ProfilePage());
     case '/detail':
-      return MaterialPageRoute(
-          builder: (_) => CategoryDetailPage(settings.arguments as Category));
+      return _buildPageWithSlideTransition(
+        () => CategoryDetailPage(settings.arguments as Category),
+        settings,
+      );
     case '/addtodo':
-      return MaterialPageRoute(
-          builder: (_) => AddTodoPage(settings.arguments as Category));
+      return _buildPageWithSlideTransition(
+          () => AddTodoPage(settings.arguments as Category), settings);
     default:
       return MaterialPageRoute(builder: (_) => SplashPage());
   }
+}
+
+Route _buildPageWithSlideTransition<T>(
+  Widget Function() builder,
+  RouteSettings settings,
+) {
+  return PageRouteBuilder(
+    transitionDuration: Duration(milliseconds: 700),
+    pageBuilder: (_, __, ___) => builder(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var curve = Curves.ease;
+      var curveTween = CurveTween(curve: curve);
+
+      var tween =
+          Tween(begin: Offset(0, 1), end: Offset(0, 0)).chain(curveTween);
+
+      var offsetAnimation = animation.drive(tween);
+      return SlideTransition(
+        position: tween
+            .animate(CurvedAnimation(parent: animation, curve: Curves.ease)),
+        child: child,
+      );
+    },
+  );
 }
